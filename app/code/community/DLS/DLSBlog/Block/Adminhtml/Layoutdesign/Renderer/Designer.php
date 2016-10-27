6f2e465dfab8473330af1db57eb14046fa9a57d6
Wayne Le <waynele@ymail.com>
2016-10-27 11:29:10 -0600

    add design tab

---
+++ app/code/community/DLS/DLSBlog/Block/Adminhtml/Layoutdesign/Renderer/Designer.php
@@ -0,0 +1,35 @@
+<?php
+
+class DLS_DLSBlog_Block_Adminhtml_Layoutdesign_Renderer_Designer extends Varien_Data_Form_Element_Textarea {
+
+    protected $_element;
+
+    public function getElementHtml() {
+
+        $this->addClass('textarea');
+        $html = '<textarea'
+                . ' id="' . $this->getHtmlId() . '"'
+                . ' name="' . $this->getName() . '"'
+                . ' style="display:none;"'
+                . '' . $this->serialize($this->getHtmlAttributes()) . ''
+                . ' >';
+        $html .= $this->getEscapedValue();
+        $html .= "</textarea>";
+        $html .= $this->getAfterElementHtml();
+
+        $elementOptions = array(
+            'id' => $this->getHtmlId(),
+            'name' => $this->getName(),
+            'html' => $this->serialize($this->getHtmlAttributes()),
+            'value' => $this->getEscapedValue(),
+            'after' => $this->getAfterElementHtml(),
+        );
+        $html .= Mage::app()->getLayout()
+                ->createBlock('dls_dlsblog/adminhtml_form_renderer_designframe')
+                ->setData('element', $elementOptions)
+                ->setTemplate('dls_dlsblog/form/renderer/layout_designer.phtml')
+                ->toHtml();
+        return $html;
+    }
+
+}
